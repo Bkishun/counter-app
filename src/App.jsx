d15e502139702f95ab01d752c6ccb542308bab20
Balkishun maurya <bkishun21072001@gmail.com>
2024-03-15 23:05:56 +0530

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+function App() {
+ let [counter, setCounter] =  useState(15)
+
+  const addValue = () =>{
+    setCounter(counter +1);
+  }
+
+  const addTwo = () =>{
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+  }
+
+  const reduceValue = () => {
+    if(counter -1 >= 0){
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <div className='middle'>
+      <div className='innerdiv'>
+      <h1>Chai Aur react</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={reduceValue}>Decrease value</button>
+      <br />
+      <button onClick={addTwo}>Add two</button>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
